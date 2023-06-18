c83fdbfa7636352d8bde208f56844102e0802253
Ujjawal Kumar <ujjawalrr@gmail.com>
2023-06-18 11:45:13 +0530

    mailing

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,9 @@ import nodemailer from 'nodemailer';
 
 export const discount = async (req, res, next) => {
   try {
-    const discount = await Discount.findOne({ code: req.body.code });
+    var code = req.body.code;
+    var codeCapital = code.toUpperCase;
+    const discount = await Discount.findOne({ code: codeCapital });
     if (discount === null) {
       res.status(400).send(`Invalid code!`)
     }
