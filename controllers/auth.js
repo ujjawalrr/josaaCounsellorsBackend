b16dba7f8aea513b82b169eca7fef22c6ae43baa
Ujjawal Kumar <ujjawalrr@gmail.com>
2023-06-18 10:17:38 +0530

    mailing

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,45 +22,29 @@ export const register = async (req, res, next) => {
     const user = await User.findOne({ email: req.body.email });
     if (user === null) {
       await newUser.save();
-      res.status(200).send(`Hey ${req.body.name}!! We are verifying your payment. We will get back to you within 24 hours. In case of any queries, raise your query in Contact Us Section or you can mail us on josaacounsellors@gmail.com .`);
-    }
+      const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+          user: 'josaacounsellors@gmail.com',
+          pass: 'knpnhgktjehhrquq'
+        }
+      })
+      const mailOptions = {
+        from: 'josaacounsellors@gmail.com',
+        to: req.body.email,
+        subject: `Registration Successful`,
+        html: `<p> Hey ${req.body.name}! Thanks for choosing us. You have successfully registered for JOSAA Counselling Helpdesk. We are verifying your payment details. You will receive confirmation mail soon.<p>`
+      }
+      transporter.sendMail(mailOptions, (error, info) => {
+        res.status(200).send(`Hey ${req.body.name}!! We are verifying your payment. We will get back to you within 24 hours. In case of any queries, raise your query in Contact Us Section or you can mail us on josaacounsellors@gmail.com .`);
+      })
+       }
     else {
       res.status(400).send(`This email is already registered with name ${req.body.name}.`);
     }
-
-    // const user = await User.findOne({ email: req.body.email });
-
-
-    // mailing
-    // const transporter = nodemailer.createTransport({
-    //   host: 'smtp.gmail.com',
-    //   port: 465,
-    //   secure: true,
-    //   auth: {
-    //     user: 'josaacounsellors@gmail.com',
-    //     pass: 'oyvziqntqpvlpdti'
-    //   }
-    // })
-
-    // const mailOptions = {
-    //   from: 'josaacounsellors@gmail.com',
-    //   to: req.body.email,
-    //   subject: `Registration Successful`,
-    //   html: `<h1> Hey ${req.body.name}!! Greetings from COMPOSIT, IIT KHARAGPUR. You have been successfully registered for COMPOSIT. Your registration id is ${req.body.regID} .<h1>`
-    // }
-
-    // transporter.sendMail(mailOptions, (error, info) => {
-    //   if (error) {
-    //     console.log(error);
-    //     res.send('error in mailing');
-    //   } else {
-    //     // res.redirect('/contact')
-    //     res.status(200).send("User has been created and mailed");
-    //   }
-    // })
-    // res.status(200).send("User has been created.");
   } catch (err) {
     res.status(400).send("Your registration was not completed. Please check all the fields and try again. If the problem persists contact us or mail us on josaacounsellors@gmail.com .");
-    // res.status(500).send(err);
   }
 };
